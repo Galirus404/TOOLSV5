2c3baac67371a92d3ec60be1091f9c32c523e8bd
Galirus_Official™ <114099161+Galirus404@users.noreply.github.com>
2024-03-03 20:27:51 +0700

    Update musik.sh

--- musik.sh
+++ musik.sh
@@ -1,4 +1,4 @@
 music_galirus=( "music.mp3" "music1.mp3" "music2.mp3" "music3.mp3" "music4.mp3" )
 random_index=$((RANDOM % ${#music_galirus[@]}))
 music_galirus="${music_galirus[random_index]}"
-play -q $HOME/TOOLSV5/sound/$music_galirus repeat vol 0.1 &>/dev/null &
+play -q $HOME/TOOLSV5/music/$music_galirus repeat vol 0.1 &>/dev/null &
